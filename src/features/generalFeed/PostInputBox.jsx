a36ccd89c52ab2e825a10a60ee098590af2dac8f
marcelosrc <marcelosrc@gmail.com>
2023-03-10 13:09:53 -0300

    Conclusão do preview de foto antes do post

--- src/features/generalFeed/PostInputBox.jsx
+++ src/features/generalFeed/PostInputBox.jsx
@@ -5,52 +5,107 @@ import generalFeedStyles from "./GeneralFeed.module.css";
 
 export default function PostInputBox() {
   const [newPost, setNewPost] = React.useState({
-    inputPost: "",
+    image: "",
+    content: "",
   });
+  const [picturePreview, setPicturePreview] = React.useState(null);
+  const authHeader = `Bearer ${localStorage.getItem("pixit")}`;
 
   const handleChange = (event) => {
-    setNewPost({ inputPost: event.target.value });
+    setNewPost({ ...newPost, content: event.target.value });
+  };
+
+  const handleFileChange = (event) => {
+    axios
+      .post(
+        "/media/post",
+        { pic: event.target.files[0] },
+        { headers: { "content-type": "multipart/form-data" } }
+      )
+      .then((res) => {
+        setNewPost({ ...newPost, image: res.data.filePath });
+        setPicturePreview(res.data.filePath);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   const handleSubmit = (event) => {
-    const authHeader = `Bearer ${localStorage.getItem("pixit")}`;
     event.preventDefault();
     axios
-      .post(
-        "/posts/create",
-        {
-          content: newPost.inputPost,
-        },
-        {
-          headers: { Authorization: authHeader },
-        }
-      )
-      .then(() => {
-        alert("Post criado!");
-        setNewPost({
-          inputPost: "",
-        });
+      .post("/posts/create", newPost, {
+        headers: { Authorization: authHeader },
       })
-      .catch((error) => {
+      .then(() => {
         setNewPost({
-          error: <small className="red">{error.response.data.message}</small>,
-          inputPost: "",
+          content: "",
+          image: "",
         });
+        setPicturePreview("");
+      })
+      .catch((err) => {
+        console.error(err);
       });
   };
+
+  const cancelImagePost = () => {
+    setNewPost({ ...newPost, image: "" });
+    setPicturePreview(null);
+  };
+
+  console.log(newPost);
+
+  const showSubmitControls = () => {
+    return (
+      <div className={generalFeedStyles["post-input-button-area"]}>
+        {picturePreview ? (
+          <button
+            className={generalFeedStyles["pixit-button"]}
+            type="submit"
+            onClick={cancelImagePost}
+          >
+            Remover Foto
+          </button>
+        ) : (
+          <label
+            type="button"
+            className={generalFeedStyles["pixit-label-button"]}
+          >
+            <input type="file" name="image" onChange={handleFileChange} />
+            {picturePreview ? "Remover Foto" : "Escolher Foto"}
+          </label>
+        )}
+        <button className={commonStyles["standard-button"]} type="submit">
+          Postar
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <form className={generalFeedStyles["post-input-box"]} onSubmit={handleSubmit}>
-      <label htmlFor="content">Qual é a polêmica de hoje?</label>
+    <form
+      className={generalFeedStyles["post-input-box"]}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="content">O que acontece?</label>
+      {picturePreview ? (
+        <div className={generalFeedStyles["post-input-picturePreview-area"]}>
+          <img
+            className={generalFeedStyles["post-input-picturePreview"]}
+            src={picturePreview}
+            alt="Preview"
+          />
+        </div>
+      ) : null}
       <input
         className={generalFeedStyles["post-input"]}
         name="content"
         type="text"
-        value={newPost.inputPost}
+        value={newPost.content}
         onChange={handleChange}
       />
-      <button className={commonStyles["standard-button"]} type="submit">
-        Postar
-      </button>
+      {showSubmitControls()}
     </form>
   );
 }
